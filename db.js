e7c18847f248dfa7af175378260d5902de8c0056
akashchlm <akashchlm95@gmail.com>
2025-11-22 12:42:15 +0530

    db.js

--- db.js
+++ db.js
@@ -1,19 +1,26 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
-const createPool = () => {
-  return mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT || 3306),
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-  });
+let pool;
+
+const getPool = () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT || 3306),
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 5,
+      maxIdle: 5,
+      idleTimeout: 60000,
+      enableKeepAlive: true,
+      keepAliveInitialDelay: 0
+    });
+  }
+  return pool;
 };
 
-const pool = createPool();
+module.exports = getPool();
 
-module.exports = pool;
 
